e46f005a90303321d95e8faada221b341ac64e8f
unknown <pablogtzgileta@gmail.com>
2020-02-24 08:26:12 -0600

    Simulation now works | Added new simulation

--- src/com/iteso/Main.java
+++ src/com/iteso/Main.java
@@ -1,15 +1,15 @@
 package com.iteso;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.Arrays;
 
 
 public class Main {
 
     public static void main(String[] args) {
         AFD afd = new AFD("sim1.txt");
-        System.out.println();
+        afd.simulate();
+
+//        System.out.println(Arrays.toString(afd.getAlphabet()));
     }
 
 }
